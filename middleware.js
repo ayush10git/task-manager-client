2129e69f2b34d3d510e15555e69c922a003cf81f
Ayush Gupta <ayushgupta.ag264@gmail.com>
2024-11-28 07:06:31 +0530

    first

--- middleware.js
+++ middleware.js
@@ -1,3 +1,11 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
 
-export const config = { matcher: ["/dashboard", "/dashboard/task"] };
+export default withAuth({
+  pages: {
+    signIn: "/",
+  },
+});
+
+export const config = {
+  matcher: ["/dashboard", "/dashboard/task"],
+};
